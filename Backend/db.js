027af76acde6c454fbb64d2cbe3462a128c576b2
Durvesh Danve <durvesh.danve@gmail.com>
2023-01-08 18:06:30 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = () => {
+    mongoose.set("strictQuery", false);                 // Used to supress annoying warning;
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to Mongo Successfully!");
     })
