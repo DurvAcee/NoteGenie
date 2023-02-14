b972ff323a2cfc3d54ec091fccf46a3b6c46d9b3
Durvesh Danve <Durvesh.Danve@veritas.com>
2023-02-14 15:01:20 +0530

    added bcryptjs & JWT

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'DurvAcee';
 
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
@@ -31,17 +35,27 @@ try {
         return res.status(400).json({error: "Sorry a user with this email already exists."})
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const secPassword = await bcrypt.hash(req.body.password, salt);
+
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: secPassword
     })
     
     // .then(user => res.json(user))
     // .catch(err => {console.log(err)
     //     res.json({error: 'Please Enter a Unique value for Email', message: err.message})
     // });
-    res.json(user)
+    const data = {
+        user:{
+            id: user.id
+        }
+    }
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json(authToken);
+
 } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error Occurred!");
