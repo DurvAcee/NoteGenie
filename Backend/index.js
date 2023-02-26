023f257d60ee243871d676563422ca7bcde7a4dc
Durvesh Danve <Durvesh.Danve@veritas.com>
2023-02-26 20:34:53 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.get('/',(req,res)=>{
-    res.send('Hello Durvesh!')
+    res.send('Greetings! Have a nice Day')
 })
 
 app.listen(port,()=>{
