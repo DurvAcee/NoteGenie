e4e60ae0eedd0d59d352b6153bed93654f838396
Durvesh Danve <Durvesh.Danve@veritas.com>
2023-03-06 00:30:18 +0530

    Delete Note added

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -87,12 +87,13 @@ const NoteState = (props) => {
       }
 
       // Delete a Note
-      const deleteNote = () => {
-        
+      const deleteNote = (id) => {
+        const newNotes = notes.filter((note) => {return note._id!==id});
+        setNotes(newNotes);
       }
 
       // Edit a Note
-      const updateNote = () => {
+      const updateNote = (id, title, description, tag) => {
         
       }
 
