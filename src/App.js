e6552b64ae4b1852c57144add1978db0b72057a9
Durvesh Danve <Durvesh.Danve@veritas.com>
2023-03-20 00:14:00 +0530

    Added Alert

--- src/App.js
+++ src/App.js
@@ -8,23 +8,36 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import { Alert } from './components/Alert';
+import Alert from './components/Alert';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import { useState } from 'react';
 
 
 function App() {
+    const [alert, setAlert] = useState(null);
+
+    const showAlert = (message, type) => {
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+    }
+
   return (
     <>
     <NoteState>
         <Router>
               <Navbar/>
-              <Alert message="This is Amazing react course"/>
+              <Alert alert={alert}/>
               <div className="container">
                 <Routes>
                     
                     <Route path="/" 
-                        element={<Home/>} >
+                        element={<Home showAlert={showAlert}/>} >
                     </Route>  
 
                     <Route exact path="/about" 
@@ -32,11 +45,11 @@ function App() {
                     </Route>  
 
                     <Route exact path="/login" 
-                        element={<Login/>} >
+                        element={<Login showAlert={showAlert}/>} >
                     </Route>  
 
                     <Route exact path="/signup" 
-                        element={<Signup/>} >
+                        element={<Signup showAlert={showAlert}/>} >
                     </Route>  
               
                 </Routes>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,7 +4,7 @@ import NoteItem from './NoteItem';
 import AddNote from './AddNote';
 
 
-const Notes = () => {
+const Notes = (props) => {
     const context = useContext(noteContext);
     const {notes, getNotes, editNote} = context;
     useEffect(() => {
@@ -27,6 +27,7 @@ const Notes = () => {
         // console.log('updating note', note);
         editNote(note.id, note.etitle, note.edescription, note.etag);
         refClose.current.click();
+        props.showAlert("Updated Successfully", "success")
 
     }
 
@@ -37,7 +38,7 @@ const Notes = () => {
 
   return (
     <>
-      <AddNote/>
+      <AddNote showAlert = {props.showAlert}/>
 
       <button type="button" className="btn btn-primary d-none" data-bs-toggle="modal" ref = {ref} data-bs-target="#exampleModal">
         Launch demo modal
@@ -86,7 +87,7 @@ const Notes = () => {
               {notes.length === 0 && 'No Notes to Display'}
           </div>'
           {notes.map((note) => {
-              return <NoteItem key = {note._id} updateNote={updateNote} note={note}/>
+              return <NoteItem key = {note._id} updateNote={updateNote} note={note} showAlert = {props.showAlert}/>
           })}
       </div>
     </>
